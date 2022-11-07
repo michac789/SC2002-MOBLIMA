94d8cfc937aff79729fec4ed11cfff066b1b7fa8
kristianachwan <kristianachwan@gmail.com>
2022-11-07 16:54:08 +0800

    boilerplate dao

--- src/DAO/AdminDAO.java
+++ src/DAO/AdminDAO.java
@@ -2,30 +2,6 @@ package DAO;
 
 import java.io.IOException;
 
-public class AdminDAO extends DAO {
-    public void createAdmin(String username, String password){
-
-        String path = "database/AdminUsers.csv";
-        try {
-            openFile(path, true);
-            writeText(username + "," + password);
-            System.out.println("Successfully create the account");
-            closeFile();
-        } catch (IOException e) {
-            // need to give error message
-            System.out.println(e.getMessage());
-            System.out.println("Unable to create admin due to unexpected error.");
-        }
-
-
-    }
-
-
-
-
-
-
-
-
-
+public class AdminDAO  {
+    
 }
